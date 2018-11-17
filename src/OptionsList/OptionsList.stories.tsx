df2863103d09ee516d1b7ae16c1e5327690122a7
Michael Donahue <mtdonahue1@gmail.com>
2018-11-17 11:47:36 -0800

    fix(core): add comp

--- src/OptionsList/OptionsList.stories.tsx
+++ src/OptionsList/OptionsList.stories.tsx
@@ -2,4 +2,17 @@ import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import OptionsList from './OptionsList';
 
-storiesOf('OptionsList', module).add('', () => <OptionsList />);
+storiesOf('OptionsList', module).add('', () => (
+  <OptionsList
+    definitions={[
+      {
+        default: 'true',
+        description: 'A test argument',
+        key: 'grid',
+        required: true,
+        type: 'string',
+        values: ['inline-flex', 'flex'],
+      },
+    ]}
+  />
+));
--- src/OptionsList/OptionsList.tsx
+++ src/OptionsList/OptionsList.tsx
@@ -1,7 +1,122 @@
+import * as classnames from 'classnames';
+import { css } from 'emotion';
+import { arrayOf, bool, shape, string } from 'prop-types';
 import * as React from 'react';
 
-const OptionsList = () => {
-  return <div>Options List!</div>;
+interface Definition {
+  default: any;
+  description: any;
+  key: any;
+  required: boolean;
+  type: any;
+  values: any;
+}
+
+interface Props {
+  tableTitle?: string;
+  definitions: Definition[];
+}
+
+const propsList = css`
+  color: #4a6271;
+  margin: 1.5em 0;
+`;
+
+const title = css`
+  border-bottom: 1px solid #cbd8de;
+  color: #708da0;
+  font-size: 14px;
+  text-transform: uppercase;
+`;
+
+const table = css`
+  border-bottom: 1px solid #edf3f3;
+  display: flex;
+  justify-content: space-between;
+  padding: 1em 0;
+`;
+
+const column = css`
+  flex: 1;
+`;
+
+const key = css`
+  color: #334049;
+`;
+
+const PropsList = ({ tableTitle, definitions }: Props) => {
+  const sortedDefinitions = definitions.sort(
+    (a: any, b: any) => b.required - a.required
+  );
+
+  return (
+    <div className={propsList}>
+      <div className={title}>{tableTitle}</div>
+
+      {sortedDefinitions.map(argument => {
+        const isRequired = argument.required ? 'required' : 'optional';
+        const requiredClass = classnames('props-list__required', {
+          'props-list__required--true': argument.required,
+        });
+
+        return (
+          <div key={argument.key} className={table}>
+            <div className={column}>
+              <div className="props-list__key">{argument.key}</div>
+              <div className={requiredClass}>{isRequired}</div>
+              <div className="props-list__type">{argument.type}</div>
+            </div>
+            <div className={column}>
+              <code>{argument.default}</code>
+            </div>
+            <div className={column}>
+              <div className="props-list__description">
+                {argument.description}
+              </div>
+              <div className="props-list__values">
+                {argument.values && <div>Possible values:</div>}
+                {argument.values &&
+                  argument.values.map((value: any, index: any, array: any) => {
+                    if (index === array.length - 1) {
+                      return (
+                        <span>
+                          or <code>{value}</code>
+                        </span>
+                      );
+                    }
+
+                    return (
+                      <span key={argument.key}>
+                        <code>{value}</code>
+                        ,&nbsp;
+                      </span>
+                    );
+                  })}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default OptionsList;
+PropsList.propTypes = {
+  definitions: arrayOf(
+    shape({
+      default: string.isRequired,
+      description: string,
+      key: string.isRequired,
+      required: bool.isRequired,
+      type: string.isRequired,
+      values: arrayOf(string),
+    })
+  ).isRequired,
+  tableTitle: string,
+};
+
+PropsList.defaultProps = {
+  tableTitle: 'Options',
+};
+
+export default PropsList;
