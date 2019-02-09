b3299e70911591f2d4a6348a757a240747d14efc
Mike Donahue <mtdonahue1@gmail.com>
2019-02-08 23:29:30 -0800

    fix: storybook with emotion (#137)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,19 @@
 module.exports = (baseConfig, env, config) => {
+  config.resolve.extensions.push('.ts', '.tsx');
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [{ loader: require.resolve('awesome-typescript-loader') }],
+    use: [
+      {
+        loader: require.resolve('babel-loader'),
+        options: {
+          plugins: ['emotion'],
+        },
+      },
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+      },
+    ],
   });
-  config.resolve.extensions.push('.ts', '.tsx');
 
   return config;
 };
