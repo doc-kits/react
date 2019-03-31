794cdc6d1fc80e36096b59cf3c4aebac9c5fbdb8
Mike Donahue <mtdonahue1@gmail.com>
2019-03-31 15:26:58 -0700

    docs: add docz docs (#187)

--- examples/docz/src/components/withstyles-hoc/index.js
+++ examples/docz/src/components/withstyles-hoc/index.js
@@ -1,14 +1,9 @@
 import { withStyles, ColorBox } from '@doc-kits/react';
 
-export default withStyles({
-  // Overriding 'wrapper' and 'name' styles
+const styles = {
   wrapper: {
-    color: 'black',
+    borderRadius: 10,
   },
-  name: {
-    fontWeight: '400',
-    // Extending 'name' styles with 'text-transform'
-    // 'textTransform' is not in default styles
-    textTransform: 'lowercase',
-  },
-})(ColorBox);
+};
+
+export default withStyles(styles)(ColorBox);
